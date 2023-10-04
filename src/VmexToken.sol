32c7f5c7bb2a094912512738583f4ae94b01432a
mel0n <melon@vmex.finance>
2023-10-04 10:41:43 -0400

    add ccipRecieve

--- src/VmexToken.sol
+++ src/VmexToken.sol
@@ -10,14 +10,80 @@ contract VMEXToken is ERC20, CCIPReceiver {
 
 	IRouterClient internal router; 
 
+	//TODO: add modifier for ccip router only to burn/mint
+
+
+	//temp -- need to get msig address and put here
+	address owner; 
+
+	uint256 public constant MAX_TOTAL_SUPPLY = 100_000_000 * 1e18; //100 million max
+	
+	//owner will be changed to msig later
+	modifier onlyCCIPRouterOrMsig() {
+		require(msg.sender == address(router) || msg.sender == owner); 
+		_; 
+	}
+
+	event MessageReceived(
+		bytes32 latestMessageId,
+        uint64 latestSourceChainSelector,
+        address latestSender,
+        string latestMessage
+	);
+
+
 	constructor(address _router) ERC20("VMEX Token", "VMEX", 18) CCIPReceiver(_router) {
 		router = IRouterClient(_router); 	
+		owner = msg.sender; 
+		_mint(owner, MAX_TOTAL_SUPPLY);  
+	}
+	
+	//should these be public?
+	//TODO: send minted tokens to team msig
+	function mint(uint256 amount) internal onlyCCIPRouterOrMsig {
+		require(amount + totalSupply <= MAX_TOTAL_SUPPLY, "Cannot mint more than max total supply");  
+		_mint(address(this), amount); 	
+	}
+	
+	//TODO: burn minted tokens from msig
+	function burn(uint256 amount) internal onlyCCIPRouterOrMsig {
+		//can build 
+		_burn(address(this), amount); 
+	}
+
+	function _ccipReceive(
+		Client.Any2EVMMessage memory message
+	) internal override {
+		//receive ccip message from router
+		//determine if we should burn or mint based on the message	
+		bytes32 latestMessageId = message.messageId;
+        uint64 latestSourceChainSelector = message.sourceChainSelector;
+        address latestSender = abi.decode(message.sender, (address));
+        string memory latestMessage = abi.decode(message.data, (string));
+
+		//assuming that we're only ever using this for our token, 
+		//we can hardcode this as [0]
+		uint256 amount = message.destTokenAmounts[0].amount; 
+		
+		//check if we need to burn of mint from the received message
+		if (keccak256(abi.encodePacked(latestMessage)) == 
+			keccak256(abi.encodePacked("burn"))) {
+				burn(amount); 
+		} else if (keccak256(abi.encodePacked(latestMessage)) == 
+			keccak256(abi.encodePacked("mint"))) {
+				mint(amount); 
+		}
+
+        emit MessageReceived(
+            latestMessageId,
+            latestSourceChainSelector,
+            latestSender,
+            latestMessage
+        );
+
 	}
 
 
-	function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
-
-	}
 
 
 }
