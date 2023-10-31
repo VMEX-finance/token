d91c9dba4f615e63473de81de3fd7e0a55d3c446
mel0n <melon@vmex.finance>
2023-10-31 14:34:00 -0400

    fixup for chainlink

--- bridge.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-
-forge script ./script/Counter.s.sol:BridgeToken -vvv --broadcast --rpc-url $AVAX_FUJI_RPC --sig "run(address,uint8,address)" -- $1 0 $2
--- run_scripts.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-
-#deploy eth and arb token contracts
-forge script ./script/Counter.s.sol:TokenScript -vvv --broadcast --rpc-url $AVAX_FUJI_RPC --sig "run(uint8)" -- 2 &&
-forge script ./script/Counter.s.sol:TokenScript -vvv --broadcast --rpc-url $ETH_SEPOLIA_RPC --sig "run(uint8)" -- 0
