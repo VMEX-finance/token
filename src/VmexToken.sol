7b26e75a9b7e9017f62976810473299961547ee1
Filip Lasic <filip.lasic89@gmail.com>
2023-12-19 15:58:10 +0100

    solidity version 0.8.19

--- src/VmexToken.sol
+++ src/VmexToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.22;
+pragma solidity 0.8.19;
 
 import {IRouterClient} from "ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {IAny2EVMMessageReceiver} from "ccip/contracts/src/v0.8/ccip/interfaces/IAny2EVMMessageReceiver.sol";
--- test/mocks/CCIPRouterMock.sol
+++ test/mocks/CCIPRouterMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.22;
+pragma solidity 0.8.19;
 
 import {IRouterClient} from "ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
